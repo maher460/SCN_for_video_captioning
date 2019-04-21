97efc38ef0cf8f7fc6ba99bd428efa7e8aeeb05c
maher460 <maher460@gmail.com>
2019-04-21 01:44:13 -0400

    wefwef

--- run2.sh
+++ run2.sh
@@ -5,6 +5,6 @@ source ./venv/bin/activate
 export CUDA_VISIBLE_DEVICES=1
 export LD_LIBRARY_PATH=/opt/cuda-8.0-cuDNN5.1/lib64/
 
-THEANO_FLAGS=mode=FAST_RUN,device=cpu,floatX=float32 python SCN_training.py
+THEANO_FLAGS=mode=FAST_RUN,device=cuda8,floatX=float32 python SCN_training.py
 
 deactivate
