94d7050a2deaae22386468b2b14fb405ea67cb93
maher460 <maher460@gmail.com>
2019-04-21 01:08:13 -0400

    bug

--- run2.sh
+++ run2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=1
-export LD_LIBRARY_PATH=/opt/cuda-8.0-cuDNN5.1/lib64
+export LD_LIBRARY_PATH=/opt/cuda-8.0-cuDNN5.1/lib64/
 source ./venv/bin/activate
+export CUDA_VISIBLE_DEVICES=1
+export LD_LIBRARY_PATH=/opt/cuda-8.0-cuDNN5.1/lib64/
 
 THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python SCN_training.py
 
